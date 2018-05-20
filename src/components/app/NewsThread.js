1572e669e7ae4920f64bb360c1b9f7dc8c9bd10a
marchenko-ivan12 <12marchenko-ia12@users.noreply.github.com>
2018-05-20 21:11:10 +0500

    v 0.4
    Запутался в реакте, сделал список новостей, который работает и все выводит, пока сойдет без верстки

--- src/components/app/NewsThread.js
+++ src/components/app/NewsThread.js
@@ -1,9 +1,7 @@
-import { List, Button, Spin } from 'antd';
-import React, { Component } from 'react';
+import React from 'react';
 import reqwest from 'reqwest';
 import styled from 'styled-components';
 import { Flex } from 'grid-styled';
-var bodyParser = require('body-parser');
 const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 /*app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,80 +17,39 @@ var User = mongoose.model("User", nameSchema); */
 
 var obj;
 export default class LoadMoreList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:[]
+        };
+    };
 
-    state = {
-        loading: true,
-        loadingMore: false,
-        showLoadingMore: true,
-        data: [],
-    }
     componentDidMount() {
         const url = 'http://localhost:2000/urnews';
         fetch(url)
-            .then(response => response.text())
-            .then(data => obj = data)
-            .then(data => console.log(obj));
-        this.getData((res) => {
-            this.setState({
-                loading: false,
-                data: res.results,
-            });
-        });
-    }
-    getData = (callback) => {
-        reqwest({
-            url: fakeDataUrl,
-            type: 'json',
-            method: 'get',
-            contentType: 'application/json',
-            success: (res) => {
-                callback(res);
-            },
-        });
-    }
-    onLoadMore = () => {
-        this.setState({
-            loadingMore: true,
-        });
-        this.getData((res) => {
-            const data = this.state.data.concat(res.results);
-            this.setState({
-                data,
-                loadingMore: false,
-            }, () => {
-                window.dispatchEvent(new Event('resize'));
-            });
-        });
+            .then(response => response.json())
+            .then((responseData) => {
+                console.log(responseData);
+                this.setState({
+                    data: responseData
+                });
+            })
     }
     render() {
-        const { loading, loadingMore, showLoadingMore, data } = this.state;
-                    const loadMore = showLoadingMore ? (
-                    <div style={{ textAlign: 'center', marginTop: 10, height: 32, lineHeight: '32px' }}>
-                    {loadingMore && <Spin />}
-                    {!loadingMore && <Button onClick={this.onLoadMore}>loading more</Button>}
-                    </div>
-                    ) : null;
-                    return (
-                    <StyledFlex2>
-                    <List
-                    className="demo-loadmore-list"
-                    loading={loading}
-                itemLayout="horizontal"
-                loadMore={loadMore}
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item actions={[ <a>more</a>]}>
-                        <List.Item.Meta
-                            title={obj}
-                            description="Fucked up beyond any recognition"
-                        />
-                        <div>content</div>
-                    </List.Item>
-                )}
-            />
+        const list = this.state.data.map(site => (
+            <li>
+                <a href={site.url}> {site.title} </a>
+            </li>
+        ));
+        return (
+            <StyledFlex2>
+            <div>
+                <ul>
+                    {list}
+                </ul>
+            </div>
             </StyledFlex2>
-        );
-
+        )
     }
 }
 
